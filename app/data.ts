c1953ca8a3d07d78ef6fb9d4f67ae412a313cfdf
HansM7 <melchorhans5@gmail.com>
2023-11-27 17:48:41 -0500

    first commit

---
+++ app/data.ts
@@ -0,0 +1,59 @@
+interface IData {
+  id?: number;
+  link: string;
+  title: string;
+  description?: string;
+  image?: string;
+  category?: string;
+}
+
+export const data: IData[] = [
+  {
+    id: 1,
+    link: "https://medusajs.com/",
+    title: "MedusaJS",
+    description: "Plugin orientado a ecommerce",
+  },
+  {
+    id: 2,
+    link: "https://skeletonreact.com",
+    title: "Skeleton",
+    description: "Efectos de carga para tus aplicaciones",
+  },
+  {
+    id: 3,
+    link: "https://uicolors.app/create",
+    title: "Tailwind UI",
+    description: "Tailwind CSS Color Generator",
+  },
+  {
+    id: 4,
+    link: "https://www.phind.com/",
+    title: "Phin AI",
+    description: "Parecido a chatgpt pero mejor",
+  },
+  {
+    id: 5,
+    link: "https://ui.shadcn.com/",
+    title: "Shadcn UI",
+    description: "UI para aplicaciones basadas en Tailwind",
+  },
+  {
+    id: 6,
+    link: "https://app.edutin.com/",
+    title: "Edutin Academy",
+    description: "Pagina de cursos online",
+  },
+  {
+    id: 7,
+    link: "https://uiverse.io/",
+    title: "UIverse.io",
+    description: "Elementos de interfaz de usuario de código abierto.",
+  },
+  {
+    id: 8,
+    link: "https://cssgrid-generator.netlify.app/",
+    title: "Css Grid Generator",
+    description: "Generador de sitemas de rejillas grid",
+  },
+];
---
+++ app/index.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { data } from "./data";
+
+function IndexComponent() {
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      {data.map((item, index) => (
+        <div
+          className="rounded-xl p-4 flex flex-col justify-center text-center bg-slate-700 hover:bg-slate-800 transition-all text-white  "
+          key={index}
+        >
+          <div>
+            <h3 className="text-lg text-violet-300 font-bold">{item.title}</h3>
+          </div>
+          <div className="my-4">
+            <a target="_blank" className="underline" href={item.link}>
+              {item.link}
+            </a>
+          </div>
+          <div>
+            <p className="text-slate-400">{item.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default IndexComponent;
